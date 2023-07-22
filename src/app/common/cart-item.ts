0ef219dcc05847aeda80bb63607a7c37d723ee07
welcome <shivam.singhs0807@gmail.com>
2023-07-22 18:19:43 +0530

    Commit new changes

--- src/app/common/cart-item.ts
+++ src/app/common/cart-item.ts
@@ -1,14 +1,14 @@
 import { Product } from './product';
- 
+
 export class CartItem {
 
-    id: number;
+    id: string;
     name: string;
     imageUrl: string;
     unitPrice: number;
 
     quantity: number;
- 
+
     constructor(product: Product) {
         this.id = product.id;
         this.name = product.name;
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,64 +1,74 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
 import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  cartItems : CartItem[] = [];
-  totalPrice : Subject<number> = new Subject<number>();
-  totalQuantity : Subject<number> = new Subject<number>();
+
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
 
   constructor() { }
 
-  addToCart(theCartItem : CartItem){
-    //check if we already have the item in the cart
-    let alreadyExistsInCart : boolean = false;
-    let existingCartItem : CartItem = undefined;
+  addToCart(theCartItem: CartItem) {
 
-    if(this.cartItems.length > 0){
-      //find the item in the cart based on item id
-      existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id === theCartItem.id);
-      //check if we found it
+    // check if we already have the item in our cart
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = undefined;
+
+    if (this.cartItems.length > 0) {
+      // find the item in the cart based on item id
+
+      existingCartItem = this.cartItems.find( tempCartItem => tempCartItem.id === theCartItem.id );
+
+      // check if we found it
       alreadyExistsInCart = (existingCartItem != undefined);
     }
-    if(alreadyExistsInCart){
-      //increment the quantity
+
+    if (alreadyExistsInCart) {
+      // increment the quantity
       existingCartItem.quantity++;
-    }else{
-      //just add the item to array
+    }
+    else {
+      // just add the item to the array
       this.cartItems.push(theCartItem);
     }
 
-    //compute cart total price and total quantity
+    // compute cart total price and total quantity
     this.computeCartTotals();
   }
 
   computeCartTotals() {
-    let totalPriceValue : number = 0;
-    let totalQuantityValue : number = 0;
 
-    for(let currentCartItem of this.cartItems){
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for (let currentCartItem of this.cartItems) {
       totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
       totalQuantityValue += currentCartItem.quantity;
     }
 
-    //publish the new values....all subscribers will receive the new data
+    // publish the new values ... all subscribers will receive the new data
     this.totalPrice.next(totalPriceValue);
     this.totalQuantity.next(totalQuantityValue);
 
-    //log cart data for debugging purpose
+    // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
-
   }
+
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
-    console.log('contents of the cart');
-    for(let tempCartItem of this.cartItems){
+
+    console.log('Contents of the cart');
+    for (let tempCartItem of this.cartItems) {
       const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
-      console.log(`name:${tempCartItem.name}, quantity=${tempCartItem.quantity}, unitPrice=${tempCartItem.unitPrice}, subTotalPrice=${subTotalPrice}`);
+      console.log(`name: ${tempCartItem.name}, quantity=${tempCartItem.quantity}, unitPrice=${tempCartItem.unitPrice}, subTotalPrice=${subTotalPrice}`);
     }
-    console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity=${totalQuantityValue}`);
-    console.log('-----');
+
+    console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
+    console.log('----');
   }
 }
