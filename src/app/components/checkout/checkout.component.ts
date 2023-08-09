810e5376851577d2830689422e3735490bad09ff
welcome <shivam.singhs0807@gmail.com>
2023-08-09 23:19:26 +0530

    new Changes

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -4,6 +4,7 @@ import { State } from 'src/app/common/state';
 import { Country } from 'src/app/common/country';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -26,9 +27,13 @@ export class CheckoutComponent implements OnInit{
   billingAddressStates : State[];
 
   constructor(private formBuilder : FormBuilder,
-              private luv2ShopFormService : Luv2ShopFormService){}
+              private luv2ShopFormService : Luv2ShopFormService,
+              private cartService : CartService){}
 
   ngOnInit(): void {
+
+    this.reviewCartDetails();
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer : this.formBuilder.group({
         firstName : new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace]),
@@ -90,6 +95,17 @@ export class CheckoutComponent implements OnInit{
           this.countries = data;
         }
       )
+  }
+  reviewCartDetails() {
+    //subscribe to total quantity
+    this.cartService.totalQuantity.subscribe(
+      totalQuantity => this.totalQuantity = totalQuantity
+    );
+
+    //subscribe to total price
+    this.cartService.totalPrice.subscribe(
+      totalPrice => this.totalPrice = totalPrice
+    );
   }
   onSubmit(){
     console.log("Handling the submit button");
